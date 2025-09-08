5ab444af7739a0a60832d49af5b077c41ae8fced
TiagoV69 <santyvarela.03@gmail.com>
2025-09-08 17:28:50 -0500

    feat: created the Conexion class to handle the connection with the database

---
+++ pjEmployeesManagement/Data/Conexion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjEmployeesManagement.Data
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Password;
+        private static Conexion Con = null;
+
+        //Constructor de la clase
+        private Conexion()
+        {
+            this.Servidor = "Santiago-Varela\\SQLEXPRESS";
+            this.Base = "bd_gestion_empleados";
+            this.Usuario = "TiagoVarela";
+            this.Password = "Mateito6900@";
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection Cadena = new SqlConnection();
+
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor +
+                                          "; Database=" + this.Base +
+                                          "; User Id=" + this.Usuario +
+                                          "; Password=" + this.Password;
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+    }
+}
