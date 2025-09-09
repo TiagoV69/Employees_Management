5fb25f0c8c2aa470f2b676dca42a91cba0967fc7
TiagoV69 <santyvarela.03@gmail.com>
2025-09-08 19:08:37 -0500

    Test: created FrmConectar form in Presentation layer to validate database connection using Conexion instance.

---
+++ pjEmployeesManagement/Presentation/FrmConectar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using pjEmployeesManagement.Data;
+
+namespace pjEmployeesManagement.Presentation
+{
+    public partial class FrmConectar : Form
+    {
+        public FrmConectar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmConectar_Load(object sender, EventArgs e)
+        {
+            SqlConnection SqlCon = new SqlConnection();
+
+            SqlCon = Conexion.crearInstancia().CreateConnection();  
+
+            try
+            {
+                SqlCon.Open();
+                MessageBox.Show("Conexión exitosa!!");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Conexión fallida :("); 
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
